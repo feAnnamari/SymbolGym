7fdb8b5b5d4ef0ca2ed290f9385c15e21cc001e8
feAnnamari <fe.annamari@gmail.com>
2024-09-17 18:03:16 +0200

    Add header

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,3 +1,41 @@
+<script setup lang="ts">
+const links = [
+    {
+        label: 'Rólunk',
+        href: '#rolunk',
+    },
+    {
+        label: 'Galéria',
+        href: '#galeria',
+    },
+    {
+        label: 'Galéria',
+        href: '#galeria',
+    },
+    {
+        label: 'Szolgáltatások',
+        href: '#szolgaltatasok',
+    },
+    {
+        label: 'Árlista',
+        href: '#arlista',
+    },
+    {
+        label: 'blog',
+        href: '#blog',
+    },
+    {
+        label: 'kapcsolat',
+        href: '#kapcsolat',
+    }
+]
+</script>
+
 <template>
-    <header>Header</header>
+    <header class="flex items-center justify-between h-24 px-10 bg-black">
+        <NuxtImg src="/img/logo_white.svg" width="64" height="64" />
+        <nav class="space-x-10">
+            <NuxtLink v-for="link in links" :to="link.href" class="tracking-widest text-white uppercase font-teko">{{ link.label }}</NuxtLink>
+        </nav>
+    </header>
 </template>
